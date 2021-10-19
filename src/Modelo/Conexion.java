ac114893a309c925e7d257b63c094de115ac1dff
Jose Gonzalez <jlgr1994@gmail.com>
2021-10-18 21:16:18 -0500

    Actualizacion Conexion a la Base de Datos

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
  *
  * @author Jose L
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Conexion {
+    Connection con;
+    public Connection getConnection(){
+        try {
+            String myBD = "jdbc:mysql://localhost:3307/sistemaventa?serverTimezone=UTC";
+            con = DriverManager.getConnection(myBD, "root", "Millos#1946");
+            return con;
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return null;
+    }
     
 }
